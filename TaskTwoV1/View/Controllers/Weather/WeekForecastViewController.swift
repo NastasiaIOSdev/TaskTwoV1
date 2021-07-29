91a4180b02229dacfe5a6d9483fd1b0fc619d4e6
NastasiaLarina <stucha84@mail.ru>
2021-07-29 14:00:19 +0500

    design changes

--- TaskTwoV1/View/Controllers/Weather/WeekForecastViewController.swift
+++ TaskTwoV1/View/Controllers/Weather/WeekForecastViewController.swift
@@ -7,23 +7,15 @@
 
 import UIKit
 
-class WeekForecastViewController: UITableViewController {
+class WeekForecastViewController: UIViewController {
 
+    @IBOutlet weak var tableview: UITableView!
+    @IBOutlet weak var weatherView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        weatherView.layer.cornerRadius = 20
     }
 
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
-    }
 
 }
