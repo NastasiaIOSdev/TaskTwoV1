476ee916064c78b3dc5b7e9f0973c9d791cffb80
NastasiaLarina <stucha84@mail.ru>
2021-08-16 19:28:27 +0500

    design customization

---
+++ TaskTwoV1/View/WeatherCells/WeekForecastCell.swift
@@ -0,0 +1,23 @@
+//
+//  WeekForecastCell.swift
+//  TaskTwoV1
+//
+//  Created by Анастасия Ларина on 16.08.2021.
+//
+
+import UIKit
+
+class WeekForecastCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
